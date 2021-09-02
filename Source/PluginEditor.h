f4d2c75685c06f552f12efba0fd996809944f2cf
mdelgado509 <mdelgado509@gmail.com>
2021-09-02 10:30:59 -0400

    Connected GUI sliders to filter params

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -40,12 +40,24 @@ private:
     
     // Sliders
     CustomRotarySlider peakFreqSlider,
-    peakGainSlider,
-    peakQualitySlider,
-    lowCutFreqSlider,
-    highCutFreqSlider,
-    lowCutSlopeSlider,
-    highCutSlopeSlider;
+                       peakGainSlider,
+                       peakQualitySlider,
+                       lowCutFreqSlider,
+                       highCutFreqSlider,
+                       lowCutSlopeSlider,
+                       highCutSlopeSlider;
+    
+    using APVTS = juce::AudioProcessorValueTreeState;
+    using Attachment = APVTS::SliderAttachment;
+    
+    Attachment peakFreqSliderAttachment,
+               peakGainSliderAttachment,
+               peakQualitySliderAttachment,
+               lowCutFreqSliderAttachment,
+               highCutFreqSliderAttachment,
+               lowCutSlopeSliderAttachment,
+               highCutSlopeSliderAttachment;
+
     
     std::vector<juce::Component*> getComps();
 
